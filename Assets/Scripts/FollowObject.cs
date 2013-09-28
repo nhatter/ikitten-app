984ed6222c0016a23c4fbcc8bb1fed519cdad1e4
Nick Hatter <nickhatter@gmail.com>
2013-09-28 15:14:08 +0100

    Add simple ball of wool

---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowObject : MonoBehaviour {
+	public GameObject targetObject;
+	public Vector3 offset;
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = targetObject.transform.position + offset;
+	}
+}
