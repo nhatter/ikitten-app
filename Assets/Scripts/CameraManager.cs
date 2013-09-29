cc909c554fdb7486d42d09fdbceecb623e200f4a
Nick Hatter <nickhatter@gmail.com>
2013-09-30 00:57:38 +0100

    Add basic GUI and allow camera to switch views

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : MonoBehaviour {
+	public static CameraManager use;
+	
+	GameObject iKitty;
+	Camera[] cameras;
+	int cameraIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		cameras = Camera.allCameras;
+		
+		// Disable all other cameras
+		foreach(Camera cam in cameras) {
+			cam.enabled = false;
+		}
+		cameras[0].enabled = true;
+		
+		iKitty = GameObject.Find("iKitty");
+		
+		use = this;
+	}
+	
+	public void nextCamera() {
+		cameras[cameraIndex].enabled = false;
+		cameraIndex++;
+		
+		if(cameraIndex == cameras.Length) {
+			cameraIndex = 0;
+		}
+		
+		cameras[cameraIndex].enabled = true;
+	}
+	
+	void Update() {
+		//Camera.main.transform.LookAt(new Vector3(Camera.main.transform.position.x, iKitty.transform.position.y, iKitty.transform.position.z));
+	}
+}
---
+++ Assets/Scripts/iKittenGUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class iKittenGUI : MonoBehaviour {
+	public GUISkin customSkin;
+	private Rect cameraButtonPos;
+	
+	
+	void Start() {
+		DontDestroyOnLoad(this);
+		int cameraButtonWidth = (int)customSkin.GetStyle("CameraButton").fixedWidth;
+		int cameraButtonHeight = (int)customSkin.GetStyle("CameraButton").fixedHeight;
+		cameraButtonPos = new Rect(Screen.width - cameraButtonWidth, 0, cameraButtonWidth, cameraButtonHeight);
+	}
+	
+	void OnGUI() {
+		GUI.skin = customSkin;
+		if(GUI.Button(cameraButtonPos, "", "CameraButton")) {
+			Debug.Log ("Changing camera");
+			CameraManager.use.nextCamera();
+		}
+	}
+}
