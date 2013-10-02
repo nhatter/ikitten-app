56f1594d747150e23fb60f0f9c157eb730ff606f
Nick Hatter <nickhatter@gmail.com>
2013-10-02 22:27:53 +0100

    Only meow if not eating and at eatLocation
    Also doesn't call Vibrate unless running on mobile

--- Assets/Scripts/iKittenModel.cs
+++ Assets/Scripts/iKittenModel.cs
@@ -74,9 +74,11 @@ public class iKittenModel : MonoBehaviour {
 		
 		isIdle = (stateInfo.nameHash == Animator.StringToHash("Base.A_idle"));
 		
+		#if UNITY_IPHONE || UNITY_ANDROID
 		if(animator.GetBool("Lick")) {
 			Handheld.Vibrate();
 		}
+		#endif
 		
 		if(isEating && Food.use.foodLevel > 0) {
 			if(timer >= timeTilSatiationIncrease) {
@@ -219,10 +221,10 @@ public class iKittenModel : MonoBehaviour {
 			waypointController.MoveToWaypoint();
 		}
 		
-		if(other.gameObject == eatLocation) {
+		if(other.gameObject == eatLocation && !isEating) {
 			Debug.Log ("At food location");
 			if(satiation <= levelToStartEating) {
-				if(!isEating && Food.use.foodLevel > 0) {
+				if(Food.use.foodLevel > 0) {
 					eat();
 				} else {
 					if(satiation > 0) {
