cd602b9f48fe983e1e80e9478855e1b1a000c7eb
Nick Hatter <nickhatter@gmail.com>
2013-10-06 11:54:20 +0100

    Add nice UI for kitten points

--- Assets/Scripts/iKittenGUI.cs
+++ Assets/Scripts/iKittenGUI.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class iKittenGUI : MonoBehaviour {
 	public GUISkin customSkin;
+	public Vector2 SHADOW_OFFSET = new Vector2(-1,-1);
 	private Rect cameraButtonPos;
 	
 	
@@ -20,6 +21,16 @@ public class iKittenGUI : MonoBehaviour {
 			CameraManager.use.nextCamera();
 		}
 		
-		GUI.Label(new Rect(Screen.width/2, 5, Screen.width/2, 40), "Points: "+PlayerModel.use.happyPoints);
+		GUI.Label(new Rect(Screen.width/4 - GUI.skin.GetStyle("iKittenIcon").fixedWidth, 5, GUI.skin.GetStyle("iKittenIcon").fixedWidth, GUI.skin.GetStyle("iKittenIcon").fixedHeight), "", "iKittenIcon");
+		dropShadowLabel(new Rect(Screen.width/4, 5, GUI.skin.GetStyle("Label").fixedWidth, GUI.skin.GetStyle("Label").fixedHeight), ""+PlayerModel.use.happyPoints, "Label");
+	}
+	
+	Color currentGUIColor;
+	void dropShadowLabel(Rect pos, string content, GUIStyle style) {
+		currentGUIColor = GUI.color;
+		GUI.color = Color.black;
+		GUI.Label(new Rect(pos.x+SHADOW_OFFSET.x, pos.y+SHADOW_OFFSET.y, pos.width, pos.height), content, "LabelShadow");
+		GUI.color = currentGUIColor;
+		GUI.Label(pos, content, style);
 	}
 }
