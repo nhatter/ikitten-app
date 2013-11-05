46ca6b8462a12e39324baf86783c1a723910a817
Nick Hatter <nickhatter@gmail.com>
2013-11-05 21:41:23 +0000

    Fix 3D text zbuffer issue

---
+++ Assets/Scripts/Text3D.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Text3D : MonoBehaviour {
+
+    void Start () {
+        renderer.material.mainTexture = this.GetComponent<TextMesh>().font.material.mainTexture;
+		renderer.material.color = this.GetComponent<TextMesh>().color;
+    }
+}
