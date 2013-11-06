4f221455453c94bb49458b7f3909f1922baf6ebc
Nick Hatter <nickhatter@gmail.com>
2013-11-06 20:12:37 +0000

    Use live server if not a development build

--- Assets/Scripts/WebConfig.cs
+++ Assets/Scripts/WebConfig.cs
@@ -2,8 +2,22 @@
 using System.Collections;
 
 public class WebConfig : MonoBehaviour {
-	public static string CREATE_USERNAME_URL = "http://localhost:8888/gdxbackend/user.php";
-    public static string VOTE_URL = "http://localhost:8888/gdxbackend/vote.php";
-    public static string FEATURES_URL = "http://localhost:8888/gdxbackend/feature_list.php";
-	public static string SUGGEST_URL ="http://localhost:8888/gdxbackend/suggest.php";
+	
+	public static string CREATE_USERNAME_URL;
+    public static string VOTE_URL;
+    public static string FEATURES_URL;
+	public static string SUGGEST_URL;
+	
+	static WebConfig() {
+		string hostname = "gamerdevx.com";
+		#if UNITY_EDITOR
+			hostname = "localhost:8888";
+			Debug.Log("Using localhost for development");
+		#endif
+		
+		CREATE_USERNAME_URL = "http://"+hostname+"/gdxbackend/user.php";
+		VOTE_URL = "http://"+hostname+"/gdxbackend/vote.php";
+		FEATURES_URL = "http://"+hostname+"/gdxbackend/feature_list.php";
+		SUGGEST_URL ="http://"+hostname+"/gdxbackend/suggest.php";
+	}
 }
